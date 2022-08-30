dd0112334972749219023097c75259b335b2d0d4
lgf2111 <lgf2111@gmail.com>
2022-08-30 16:34:13 +0800

    update style

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,9 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 
 app = Flask(__name__)
+
+# Configurations
 app.config['SECRET_KEY'] = '13050e1c636532b3237d222fcd7fd944'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initializations
 db = SQLAlchemy(app)
 admin = Admin(app, name='Energy Calculator', template_mode='bootstrap4')
 
