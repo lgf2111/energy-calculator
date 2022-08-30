9882b17ba43fab280104eb9e6ea45da9bdf908da
lgf2111 <lgf2111@gmail.com>
2022-08-30 16:30:19 +0800

    models

--- webapp/forms.py
+++ webapp/forms.py
@@ -3,7 +3,7 @@ from wtforms import SelectField
 from wtforms.validators import DataRequired
 
 class CalculatorForm(FlaskForm):
-    appliance = SelectField('Appliance', choices=[('Washing Machine', 'Refrigerator', 'Television', 'Air-conditioning')], validators=DataRequired())
-    brand = SelectField('Brand Name', choices=[('Panasonic', 'Samsung', 'Mitsubishi', 'Daikin', 'LG',  'Philips', 'Hitachi', )], validators=DataRequired())
-    ticks = SelectField('Efficiency Level', choices=[(1, 2, 3, 4, 5)], validators=DataRequired())
-    time_used = SelectField('Duration Used', choices=[('Less than 1h', '2h', '3h', 'More than 3h')], validators=DataRequired())
+    appliance = SelectField('Appliance', choices=[('Washing Machine', 'Refrigerator', 'Television', 'Air-conditioning')], validators=[DataRequired()])
+    brand = SelectField('Brand Name', choices=[('Panasonic', 'Samsung', 'Mitsubishi', 'Daikin', 'LG',  'Philips', 'Hitachi', )], validators=[DataRequired()])
+    ticks = SelectField('Efficiency Level', choices=[(1, 2, 3, 4, 5)], validators=[DataRequired()])
+    time_used = SelectField('Duration Used', choices=[('Less than 1h', '2h', '3h', 'More than 3h')], validators=[DataRequired()])
---
+++ webapp/models.py
@@ -0,0 +1,21 @@
+from webapp import db
+from datetime import datetime
+
+class Appliance(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    watts = db.Column(db.Integer, nullable=False)
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    brand_id = db.Column(db.Integer, db.ForeignKey('brand.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Appliance('{self.name}', '{self.watts}', '{self.date_added}')"
+
+
+class Brand(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date_added = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    appliances = db.relationship('Appliance', backref='brand', lazy=True)
+    def __repr__(self):
+        return f"Appliance('{self.name}', '{self.watts}', '{self.date_added}')"
