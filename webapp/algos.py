11dcc98942bd7e008740f17b0c7ca61741bfabcb
Atlas2212 <shinthantteaung@gmail.com>
2022-08-30 20:59:55 +0800

    .

--- webapp/algos.py
+++ webapp/algos.py
@@ -9,78 +9,78 @@ def calculate(television, fridge, air_conditioner, washing_machine, hours):
     return total_energy, total_amount
 
 def recommend(hours):
-    television, fridge, air_conditioner, washing_machine = hours
+    television, fridge, air_conditioner, washing_machine = hours.values()
     recommendations = {}
     #tv
     if television > 4:
         recommendations["television"] = "limit tv to 4 hours"
-        savetime = hours - 4
-        saveamt = savetime * 365
-        savewatt = saveamt * television.watt
-        saveprice = savewatt * 0.3
+        # savetime = hours - 4
+        # saveamt = savetime * 365
+        # savewatt = saveamt * television.watt
+        # saveprice = savewatt * 0.3
     elif television > 2:
         recommendations["television"] =  "limit hours to 2 hours"
-        savetime = hours - 2
-        saveamt = savetime * 365
-        savewatt = saveamt * television.watt
-        saveprice = savewatt * 0.3
+        # savetime = hours - 2
+        # saveamt = savetime * 365
+        # savewatt = saveamt * television.watt
+        # saveprice = savewatt * 0.3
     elif television < 2:
         recommendations["television"] = "Great Job!"
     #fridge
     if fridge != 0:
-        print("Changing to a more efficient brand or reducing the temperature can reduce its energy consumption") 
-        saveamt = savetime * 365
-        savewatt = saveamt * 30
-        saveprice = savewatt * 0.3
+        recommendations["fridge"] = "Changing to a more efficient brand or reducing the temperature can reduce its energy consumption"
+        # saveamt = savetime * 365
+        # savewatt = saveamt * 30
+        # saveprice = savewatt * 0.3
     #aircon
     if air_conditioner > 10:
         recommendations["air_conditioner"] = "limit aircon to 8 hours"
-        savetime = hours - 10
-        saveamt = savetime * 365
-        savewatt = saveamt * air_conditioner.watt
-        saveprice = savewatt * 0.3
+        # savetime = hours - 10
+        # saveamt = savetime * 365
+        # savewatt = saveamt * air_conditioner.watt
+        # saveprice = savewatt * 0.3
     elif air_conditioner > 8:
         recommendations["air_conditioner"] = "limit aircon to 6 hours"
-        savetime = hours - 6
-        saveamt = savetime * 365
-        savewatt = saveamt * air_conditioner.watt
-        saveprice = saveamt * 0.3
+        # savetime = hours - 6
+        # saveamt = savetime * 365
+        # savewatt = saveamt * air_conditioner.watt
+        # saveprice = saveamt * 0.3
     elif air_conditioner > 6:
         recommendations["air_conditioner"] = "limit aircon to 4 hours"
-        savetime = hours - 6
-        saveamt = savetime * 365
-        savewatt = saveamt * air_conditioner.watt
-        saveprice = savewatt * 0.3
+        # savetime = hours - 6
+        # saveamt = savetime * 365
+        # savewatt = saveamt * air_conditioner.watt
+        # saveprice = savewatt * 0.3
     elif air_conditioner > 4:
         recommendations["air_conditioner"] = "limit aircon to 2 hours"
-        savetime = hours - 4
-        saveamt = savetime * 365
-        savewatt = saveamt * air_conditioner.watt
-        saveprice = savewatt * 0.3
+        # savetime = hours - 4
+        # saveamt = savetime * 365
+        # savewatt = saveamt * air_conditioner.watt
+        # saveprice = savewatt * 0.3
     elif air_conditioner > 2 :
         recommendations["air_conditioner"] = "limit aircon to 2 hours"
-        savetime = hours - 2
-        saveamt = savetime * 365
-        savewatt = saveamt * air_conditioner.watt
-        saveprice = savewatt * 0.3
+        # savetime = hours - 2
+        # saveamt = savetime * 365
+        # savewatt = saveamt * air_conditioner.watt
+        # saveprice = savewatt * 0.3
     elif air_conditioner <= 2:
         recommendations["air_conditioner"] = "Good Job!"
     #wm
     if washing_machine > 3:
         recommendations["washing_machine"] = "decrease the number of times of washing to 3"
-        savetime = hours - 3
-        saveamt = savetime * 365
-        savewatt = saveamt * washing_machine.watt
-        saveprice = savewatt * 0.3
+        # savetime = hours - 3
+        # saveamt = savetime * 365
+        # savewatt = saveamt * washing_machine.watt
+        # saveprice = savewatt * 0.3
     if washing_machine > 2:
         recommendations["washing_machine"] = "decrease the number of times of washing to 2"
-        savetime = hours - 2
-        saveamt = savetime * 365
-        savewatt = saveamt * washing_machine.watt
-        saveprice = savewatt * 0.3
+        # savetime = hours - 2
+        # saveamt = savetime * 365
+        # savewatt = saveamt * washing_machine.watt
+        # saveprice = savewatt * 0.3
     if washing_machine <= 2:
         recommendations["washing_machine"] = "decrease the time spent washing per load"
-        saveamt = savetime * 365
-        savewatt = saveamt * 50
-        saveprice = savewatt * 0.3
+        # saveamt = savetime * 365
+        # savewatt = saveamt * 50
+        # saveprice = savewatt * 0.3
     return recommendations
--- webapp/algos_test.py
+++ webapp/algos_test.py
@@ -11,7 +11,8 @@ def test_calculate():
 
 def test_recommend():
     hours = {"television": 2, "fridge": 2, "air_conditioner": 2, "washing_machine": 2}
-    assert recommend(hours) == {"television": "Limit television to 2 hours",
+    recommendations = recommend(hours)
+    assert recommendations == {"television": "Limit television to 2 hours",
                                 "fridge": "Changing to a more efficient brand or reducing the temperature can reduct its energy consumption",
                                 "air_conditioner": "Limit air conditioner to 2 hours",
-                                "washing_machine": "Decrease the number of times of washing to 2"}
+                                "washing_machine": "Decrease the number of times of washing to 2"}, recommendations
