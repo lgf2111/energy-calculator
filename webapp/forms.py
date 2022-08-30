5999881b0ba1999e31dce6a089b5bdcf645bd80e
Pratawala <96869075+Pratawala@users.noreply.github.com>
2022-08-30 17:59:29 +0800

    no.1

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,6 +1,6 @@
 import enum
 from flask_wtf import FlaskForm
-from wtforms import SelectField, RadioField
+from wtforms import SelectField, RadioField, IntegerField
 from wtforms.validators import DataRequired
 from webapp.models import Appliance, Brand
 
@@ -8,9 +8,9 @@ class CalculatorForm(FlaskForm):
     appliance_list = [(_.name, _.name) for _ in Appliance.query.all()]
     brand_list = [(_.name, _.name) for _ in Brand.query.all()]
     ticks_list = [(_, _) for _ in range(1,6)]
-    usage_list = [(i, v) for i, v in enumerate(["< 1h", "1h", "2h", "3h", "> 3h"])]
+   
 
     appliance = SelectField('Appliance', choices=appliance_list, validators=[DataRequired()])
     brand = SelectField('Brand Name', choices=brand_list, validators=[DataRequired()])
     ticks = RadioField('Efficiency Level', choices=ticks_list, validators=[DataRequired()])
-    usage = RadioField('Duration Used', choices=usage_list, validators=[DataRequired()])
+    usage = IntegerField('Duration Used',  validators=[DataRequired()])
