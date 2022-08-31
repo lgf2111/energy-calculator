67078bdb9100a922014a92852fd93c28eff21eab
lgf2111 <lgf2111@gmail.com>
2022-08-31 17:16:54 +0800

    update filterbrands

--- webapp/static/script/filter-brands.js
+++ webapp/static/script/filter-brands.js
@@ -1,46 +1,69 @@
-const brandList = ["Panasonic", "LG", "Samsung", "Mitsubishi", "SHARP", "Others", "Daikin", "Electrolux", "Philips"];
-const appliance = document.getElementById("appliance");
-const brand = document.getElementById("brand");
+var brandList = [
+  "Panasonic",
+  "LG",
+  "Samsung",
+  "Mitsubishi",
+  "SHARP",
+  "Others",
+  "Daikin",
+  "Electrolux",
+  "Philips",
+];
+var appliance = document.getElementById("appliance");
+var brand = document.getElementById("brand");
 
-document.addEventListener('click', () => {
+while (brand.options.length > 0) {
+    brand.remove(brand.options.length - 1);
+  }
 
-    while (appliance.options.length > 0) {
-        appliance.remove(appliance.options.length - 1)
-    }
-    switch(appliance.value) {
-        case "Air-Conditioner":
-            for (let v of ["Daikin", "Panasonic", "Electrolux", "Mitsubishi", "Samsung", "Others"]) {
-            let option = document.createElement('option')
-            option.text = v
-            option.value = v
-            appliance.add(option, null)
-            }
-            break;
-        case "Fridge":
-            for (let v of ["Panasonic", "LG", "Hitachi", "Samsung", "Others"]) {
-            let option = document.createElement('option')
-            option.text = v
-            option.value = v
-            appliance.add(option, null)
-            }
-            break;
-        case "Television":
-            for (let v of ["Panasonic", "LG", "Philips", "Samsung", "Others"]) {
-            let option = document.createElement('option')
-            option.text = v
-            option.value = v
-            appliance.add(option, null)
-            }
-            break;
-        case "Washing Machine":
-            for (let v of ["Panasonic", "LG", "SHARP", "Samsung", "Others"]) {
-            let option = document.createElement('option')
-            option.text = v
-            option.value = v
-            appliance.add(option, null)
-            }
-            break;
-        default:
-            // code block
-        }
+appliance.addEventListener("change", () => {
+  while (brand.options.length > 0) {
+    brand.remove(brand.options.length - 1);
+  }
+  switch (appliance.value) {
+    case "Air-Conditioner":
+      for (let v of [
+        "Daikin",
+        "Panasonic",
+        "Electrolux",
+        "Mitsubishi",
+        "Samsung",
+        "Others",
+      ]) {
+        let option = document.createElement("option");
+        option.text = v;
+        option.value = v;
+        brand.add(option, null);
+      }
+      break;
+    case "Fridge":
+      for (let v of ["Panasonic", "LG", "Hitachi", "Samsung", "Others"]) {
+        let option = document.createElement("option");
+        option.text = v;
+        option.value = v;
+        brand.add(option, null);
+      }
+      break;
+    case "Television":
+      for (let v of ["Panasonic", "LG", "Philips", "Samsung", "Others"]) {
+        let option = document.createElement("option");
+        option.text = v;
+        option.value = v;
+        brand.add(option, null);
+      }
+      break;
+    case "Washing Machine":
+      for (let v of ["Panasonic", "LG", "SHARP", "Samsung", "Others"]) {
+        let option = document.createElement("option");
+        option.text = v;
+        option.value = v;
+        brand.add(option, null);
+      }
+      break;
+    default:
+      option = document.createElement("option");
+      option.text = "Please choose appliance first!";
+      option.value = "invalid";
+      brand.add(option, null);
+  }
 });
