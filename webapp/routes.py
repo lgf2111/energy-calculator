f144661f52d2e03a491a31717c635d50a26e6080
Atlas2212 <shinthantteaung@gmail.com>
2022-08-31 14:50:25 +0800

    .

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,6 +1,7 @@
 from webapp import app, db
 from flask import render_template, redirect, url_for, flash
 from webapp.forms import CalculatorForm
+import matplotlib.pyplot as plt
 
 @app.route('/')
 @app.route('/home')
