b238535d96ab38dcb631593f48aa43ce6247d85b
lgf2111 <lgf2111@gmail.com>
2022-09-01 22:57:12 +0800

    updated buttons

--- webapp/static/script/forms.js
+++ webapp/static/script/forms.js
@@ -52,7 +52,7 @@ function addForm() {
         })()}
     </div>
     <div class="remove text-right">
-        <button type="button" class="btn btn-danger bi bi-trash-fill" onclick= "deleteForm(${i})"> Remove</button>
+        <button type="button" class="btn btn-danger" onclick= "deleteForm(${i})"><i class="bi bi-file-earmark-minus"></i> Remove</button>
     </div>
     `
     document.getElementById("index").value = i
